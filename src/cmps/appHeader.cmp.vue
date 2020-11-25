7782fd63d733d648151cf09f84c4c7b7b2289aa3
Zohar Sela <zohar10692@gmail.com>
2020-11-25 16:25:08 +0200

    home page

---
+++ src/cmps/appHeader.cmp.vue
@@ -0,0 +1,17 @@
+<template>
+<header class="main-header flex f-col f-center full">
+  <a href="https://www.gov.il/he/departments/topics/corona-main-sub?gclid=CjwKCAiAnvj9BRA4EiwAuUMDfwqvMW-aXce0UhFerpxXrTDXyF83RoPmpQ9wdyf64GjhtbFrLMShKhoCjW8QAvD_BwE"  target="_blank">Get the latest on our COVID-19 response</a>
+  <nav class="header-nav flex">
+ <router-link to="/" exact class="logo-navbar">MiCasa</router-link>
+  <router-link to="/house" exact>Places to stay</router-link>
+  <router-link to="/login" exact>Become a host</router-link>
+  </nav>
+</header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
