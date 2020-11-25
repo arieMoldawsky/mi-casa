e62228c8a1c9b6e253b916422fcd21c8f9c644b2
Tal Harouvi <halouvi@gmail.com>
2020-11-25 11:08:31 +0200

    Good Luck!

--- src/services/house.service.js
+++ src/services/house.service.js
@@ -1,61 +1,108 @@
-import { utilService } from './util.service.js';
+import { utilService } from './util.service.js'
 import httpService from './http.service'
-import axios from 'axios';
+import axios from 'axios'
 
-
-const baseUrl = (process.env.NODE_ENV !== 'development') ? '/api/house' : '//localhost:3000/api/house';
-
+const baseUrl =
+  process.env.NODE_ENV !== 'development'
+    ? '/api/house'
+    : '//localhost:3000/api/house'
 
 export const houseService = {
-    query,
-    getEmptyHouse,
-    findById,
-    save,
-    remove,
+  query,
+  getEmptyHouse,
+  findById,
+  save,
+  remove,
 }
 
 function query(query) {
-    var queryURL = `?`
-    for (const key in query) {
-        if (query[key]) queryURL += `${key}=${query[key]}&`
-    }
-    return httpService.get(`house/${queryURL}`)
+  var queryURL = `?`
+  for (const key in query) {
+    if (query[key]) queryURL += `${key}=${query[key]}&`
+  }
+  return httpService.get(`house/${queryURL}`)
 }
 
 function query(q = {}) {
-    return httpService.get(`house?name=${q.txt}&inStock=${q.inStock}`)
+  return httpService.get(`house?name=${q.txt}&inStock=${q.inStock}`)
 }
 
 function getEmptyHouse() {
-    return {
-        _id: '',
-        title: '',
-        isDone: false
-    }
+  return {
+    _id: '',
+    title: '',
+    isDone: false,
+  }
 }
 
 function findById(houseId) {
-    return httpService.get(`house/${houseId}`)
+  return httpService.get(`house/${houseId}`)
 }
 
 function remove(house) {
-    return httpService.delete(`house/${house._id}`)
+  return httpService.delete(`house/${house._id}`)
 }
 
 function save(house) {
-    if (house._id) {
-        return httpService.put(`house/${house._id}`, house)
-    } else {
-        return _add(house)
-            .then(house => house)
-    }
+  if (house._id) {
+    return httpService.put(`house/${house._id}`, house)
+  } else {
+    return _add(house).then(house => house)
+  }
 }
-
 
 function _add(house) {
-    // house._id = utilService.makeId();
-    // house.type = 'Funny';
-    // house.createdAt = Date.now();
-    return axios.post(`${baseUrl}`, house)
-        .then(res => res.data)
+  // house._id = utilService.makeId();
+  // house.type = 'Funny';
+  // house.createdAt = Date.now();
+  return axios.post(`${baseUrl}`, house).then(res => res.data)
 }
+
+const houses = [
+  {
+    _id: '5c09',
+    name: 'Funky Monks',
+    price: 50,
+    imgs: [
+      'http://someimg1.jpeg',
+      'http://someimg2.jpeg',
+      'http://someimg3.jpeg',
+    ],
+    'maxGuests/ capacity': 4,
+    bedRooms: 3,
+    tags: ['Funk', 'Happy'],
+    amenities: ['Wifi', 'Washer'],
+    description: 'lorem',
+    location: {
+      lat: 32.21,
+      lng: 22.11,
+      country: 'Israel',
+      city: 'Tel-Aviv',
+    },
+    host: {
+      _id: 'u105',
+      fullName: 'Puki Ben David',
+      imgUrl: 'http://some-photo/',
+    },
+    reviews: [
+      {
+        id: 'u101',
+        text: 'asdasd',
+        rating: 4,
+        user: {
+          _id: 'u101',
+          fullName: 'Orly Amdadi',
+          imgUrl: 'http://some-img',
+        },
+      },
+    ],
+    chat: [
+      {
+        id: 'asd23',
+        fromId: 'asd',
+        txt: 'lorem',
+        sentAt: 16769958698756,
+      },
+    ],
+  },
+]
---
+++ src/store/house.store.js
@@ -0,0 +1,99 @@
+import { houseService } from '@/services/house.service.js'
+import { imgUploadService } from '@/services/img.upload.service.js'
+import { eventBus, SHOW_MSG } from '@/services/eventBus.service.js'
+
+export default {
+  state: {
+    filterBy: {
+      page: 1,
+      limit: 10,
+      txt: '',
+      sortBy: 'name',
+      tags: 'all',
+    },
+    houses: [],
+    housesTotalCount: null,
+    isLoading: false,
+  },
+  getters: {
+    filterBy({ filterBy }) {
+      return filterBy
+    },
+  },
+  actions: {
+    async loadHouses(context, { type }) {
+      context.commit({ type: 'setIsLoading', isLoading: true })
+      try {
+        const res = await houseService.query(context.state.filterBy)
+        context.commit({ type, res })
+        context.commit({ type: 'setIsLoading', isLoading: false })
+      } catch (error) {
+        console.log('ERROR: could not load houses')
+      }
+    },
+    async loadHouse(context, { type, houseId }) {
+      try {
+        const house = await houseService.getById(houseId)
+        return house
+      } catch (error) {
+        console.log('ERROR: could not load house: ', houseId)
+      }
+    },
+    async removeHouse({ dispatch }, payload) {
+      houseService
+        .remove(payload._id)
+        .then(() => dispatch({ type: 'loadHouses' }))
+        .then(() =>
+          eventBus.$emit(SHOW_MSG, {
+            txt: `${payload._id} Removed Succefully`,
+            type: 'success',
+          })
+        )
+    },
+    async addHouse(context, { house, img }) {
+      try {
+        const { url } = await imgUploadService.uploadImg(img)
+        house.img = url
+        const houseRes = await houseService.add(house)
+        eventBus.$emit(SHOW_MSG, {
+          txt: `${houseRes._id} Added Succefully`,
+          type: 'success',
+        })
+      } catch (error) {
+        console.log(error)
+      }
+    },
+    async updateHouse({ dispatch }, payload) {
+      houseService
+        .update(payload.house)
+        .then(() => dispatch({ type: 'loadHouses' }))
+        .then(() =>
+          eventBus.$emit(SHOW_MSG, {
+            txt: `${payload.house._id} Updated Succefully`,
+            type: 'success',
+          })
+        )
+    },
+    updateFilter({ commit }, payload) {
+      commit(payload)
+    },
+    setIsLoading({ commit }, payload) {
+      commit(payload)
+    },
+  },
+  mutations: {
+    loadHouses(state, { res }) {
+      state.houses = res.houses
+      state.housesTotalCount = res.housesLength
+    },
+    // loadHouse(state, { house }) {
+    //   state.house = house
+    // },
+    updateFilter(state, { filterBy }) {
+      state.filterBy = { ...filterBy }
+    },
+    setIsLoading(state, { isLoading }) {
+      state.isLoading = isLoading
+    },
+  },
+}
