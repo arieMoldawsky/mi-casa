2ce970e25e7c1f75bee41330660834228577c870
Zohar Sela <zohar10692@gmail.com>
2020-11-26 10:44:14 +0200

    bla

--- src/cmps/housePreview.cmp.vue
+++ src/cmps/housePreview.cmp.vue
@@ -29,16 +29,7 @@
       <h4>{{ houseDescription }}</h4>
       <h4>${{ house.price }} / night</h4>
     </div>
-  <section class="house-card flex-start column">
-    <!-- <div class="house-card-container"> -->
-    <img :src="house.imgs[0]" alt="" />
-    <!-- <div>{{ house.imgs }}</div> -->
-    <span>{{ house.reviews.rating }}</span>
-    <span>{{ house.type }} · {{ house.location.city }}</span>
-    <!-- <h4>{{ house.description }}</h4> -->
-    <h4>${{ house.price }}</h4>
-    <!-- </div> -->
-  </section>
+    </section>
 </template>
 
 <script>
--- src/main.js
+++ src/main.js
@@ -4,20 +4,20 @@ import router from './router'
 import store from './store'
 import element from 'element-ui'
 import locale from 'element-ui/lib/locale/lang/en'
-// import * as VueGoogleMaps from 'vue2-google-maps';
+import * as VueGoogleMaps from 'vue2-google-maps';
 
 import '@/styles/styles.scss'
 
-// Vue.use(VueGoogleMaps, {
-//   load: {
-//     key: 'AIzaSyB0b3on9pGxutkQG8-bRNmspW4imoC7_OI',
-//     libraries: 'places',
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: 'AIzaSyB0b3on9pGxutkQG8-bRNmspW4imoC7_OI',
+    libraries: 'places',
     
-//   },
-// });
+  },
+});
 
-// Vue.use(element, {locale})
-// Vue.config.productionTip = false
+Vue.use(element, {locale})
+Vue.config.productionTip = false
 
 new Vue({
   router,
