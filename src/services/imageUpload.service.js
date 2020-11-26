822404ed84ce40f211fc415c36449dcb6c027a80
Tal Harouvi <halouvi@gmail.com>
2020-11-26 21:36:56 +0200

    asd

--- src/services/imageUpload.service.js
+++ src/services/imageUpload.service.js
@@ -1,18 +1,18 @@
-import axios from 'axios';
+import axios from 'axios'
 
-export const uploadImg = async (ev) => {
-    const UPLOAD_PRESET = 'iuzstnbj';
-    const CLOUD_NAME = 'ariecloud';
-    const UPLOAD_URL = `https://api.cloudinary.com/v1_1/${CLOUD_NAME}/image/upload`;
-    const FORM_DATA = new FormData();
+export const uploadImg = async ev => {
+  const UPLOAD_PRESET = 'iuzstnbj'
+  const CLOUD_NAME = 'ariecloud'
+  const UPLOAD_URL = `https://api.cloudinary.com/v1_1/${CLOUD_NAME}/image/upload`
+  const FORM_DATA = new FormData()
 
-    FORM_DATA.append('file', ev.target.files[0]);
-    FORM_DATA.append('upload_preset', UPLOAD_PRESET);
+  FORM_DATA.append('file', ev.target.files[0])
+  FORM_DATA.append('upload_preset', UPLOAD_PRESET)
 
-    try {
-        const res = await axios.post(UPLOAD_URL, FORM_DATA)
-        return res.data;
-    } catch(err) {
-        console.log('ERROR:', err);
-    }
+  try {
+    const { data } = await axios.post(UPLOAD_URL, FORM_DATA)
+    return data
+  } catch (err) {
+    console.log('ERROR:', err)
+  }
 }
