abf837f23c9727705d1d28251f7ffa21d5bd578f
Zohar Sela <zohar10692@gmail.com>
2020-11-29 22:43:24 +0200

    commit

--- src/cmps/appHeader.cmp.vue
+++ src/cmps/appHeader.cmp.vue
@@ -19,9 +19,9 @@
         <el-row class="block-col-2">
           <el-col :span="12">
             <el-dropdown trigger="click" @command="handleCommand">
-              <div class="el-dropdown-link-container">
-                <div class="el-dropdown-link">
-                  <div class="hamburgar">
+              <div class="el-dropdown-link-container block">
+                <div class="el-dropdown-link inline">
+                  <div class="hamburgar inline-flex">
                     <svg
                       viewBox="0 0 32 32"
                       xmlns="http://www.w3.org/2000/svg"
@@ -37,8 +37,8 @@
                       </g></svg
                     ><i class="el-icon-arrow-down el-icon--right"></i>
                   </div>
-                  <div class="profile-pic">
-                    <!-- <svg class="profile-pic" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="presentation" focusable="false" style="display: block; fill: currentcolor;"><path d="m16 .7c-8.437 0-15.3 6.863-15.3 15.3s6.863 15.3 15.3 15.3 15.3-6.863 15.3-15.3-6.863-15.3-15.3-15.3zm0 28c-4.021 0-7.605-1.884-9.933-4.81a12.425 12.425 0 0 1 6.451-4.4 6.507 6.507 0 0 1 -3.018-5.49c0-3.584 2.916-6.5 6.5-6.5s6.5 2.916 6.5 6.5a6.513 6.513 0 0 1 -3.019 5.491 12.42 12.42 0 0 1 6.452 4.4c-2.328 2.925-5.912 4.809-9.933 4.809z"></path></svg> -->
+                  <div class="profile-pic inline-flex">
+                    <svg class="profile-pic" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="presentation" focusable="false" style="display: block; fill: currentcolor;"><path d="m16 .7c-8.437 0-15.3 6.863-15.3 15.3s6.863 15.3 15.3 15.3 15.3-6.863 15.3-15.3-6.863-15.3-15.3-15.3zm0 28c-4.021 0-7.605-1.884-9.933-4.81a12.425 12.425 0 0 1 6.451-4.4 6.507 6.507 0 0 1 -3.018-5.49c0-3.584 2.916-6.5 6.5-6.5s6.5 2.916 6.5 6.5a6.513 6.513 0 0 1 -3.019 5.491 12.42 12.42 0 0 1 6.452 4.4c-2.328 2.925-5.912 4.809-9.933 4.809z"></path></svg>
                   </div>
                 </div>
               </div>
