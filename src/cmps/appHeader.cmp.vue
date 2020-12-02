61154cd6e6bbc03c451bb0e39454f0afbf76b2f4
Arie Moldawsky <ariemol555@gmail.com>
2020-12-02 16:56:26 +0200

    header hamburger rotate

--- src/cmps/appHeader.cmp.vue
+++ src/cmps/appHeader.cmp.vue
@@ -35,71 +35,74 @@
         <router-link to="/profile" exact class="host-navbar"
           >Become a host
         </router-link>
-        <el-row class="block-col-2">
-          <el-col :span="12">
-            <el-dropdown trigger="click" @command="handleCommand">
-              <div class="el-dropdown-link-container flex">
-                <div class="el-dropdown-link flex">
-                  <div class="hamburgar flex">
-                    <svg
-                      viewBox="0 0 32 32"
-                      xmlns="http://www.w3.org/2000/svg"
-                      aria-hidden="true"
-                      role="presentation"
-                      focusable="false"
-                      style="display: block; height: 16px; width: 16px;  stroke-width: 3; overflow: visible;"
-                    >
-                      <g fill="none" fill-rule="nonzero">
-                        <path d="m2 16h28"></path>
-                        <path d="m2 24h28"></path>
-                        <path d="m2 8h28"></path>
-                      </g></svg
-                    ><i class="el-icon-arrow-down el-icon--right"></i>
+        <section @click="onDropdown">
+          <el-row class="block-col-2">
+            <el-col :span="12">
+              <el-dropdown trigger="click" @command="handleCommand">
+                <div class="el-dropdown-link-container flex">
+                  <div class="el-dropdown-link flex">
+                    <div :class="rotateBurger" class="hamburger flex">
+                      <svg
+                        
+                        viewBox="0 0 32 32"
+                        xmlns="http://www.w3.org/2000/svg"
+                        aria-hidden="true"
+                        role="presentation"
+                        focusable="false"
+                        style="display: block; height: 16px; width: 16px;  stroke-width: 3; overflow: visible;"
+                      >
+                        <g fill="none" fill-rule="nonzero">
+                          <path d="m2 16h28"></path>
+                          <path d="m2 24h28"></path>
+                          <path d="m2 8h28"></path>
+                        </g></svg
+                      ><i class="el-icon-arrow-down el-icon--right"></i>
+                    </div>
+                    <div v-if="!isLogedIn" class="profile-pic flex">
+                      <svg
+                        class="profile-pic"
+                        viewBox="0 0 32 32"
+                        xmlns="http://www.w3.org/2000/svg"
+                        aria-hidden="true"
+                        role="presentation"
+                        focusable="false"
+                        style="display: block; fill: currentcolor;"
+                      >
+                        <path
+                          d="m16 .7c-8.437 0-15.3 6.863-15.3 15.3s6.863 15.3 15.3 15.3 15.3-6.863 15.3-15.3-6.863-15.3-15.3-15.3zm0 28c-4.021 0-7.605-1.884-9.933-4.81a12.425 12.425 0 0 1 6.451-4.4 6.507 6.507 0 0 1 -3.018-5.49c0-3.584 2.916-6.5 6.5-6.5s6.5 2.916 6.5 6.5a6.513 6.513 0 0 1 -3.019 5.491 12.42 12.42 0 0 1 6.452 4.4c-2.328 2.925-5.912 4.809-9.933 4.809z"
+                        ></path>
+                      </svg>
+                    </div>
+                    <img class="profile-img" v-else :src="isLogedIn.imgUrl" />
                   </div>
-                  <div v-if="!isLogedIn" class="profile-pic flex">
-                    <svg
-                      class="profile-pic"
-                      viewBox="0 0 32 32"
-                      xmlns="http://www.w3.org/2000/svg"
-                      aria-hidden="true"
-                      role="presentation"
-                      focusable="false"
-                      style="display: block; fill: currentcolor;"
-                    >
-                      <path
-                        d="m16 .7c-8.437 0-15.3 6.863-15.3 15.3s6.863 15.3 15.3 15.3 15.3-6.863 15.3-15.3-6.863-15.3-15.3-15.3zm0 28c-4.021 0-7.605-1.884-9.933-4.81a12.425 12.425 0 0 1 6.451-4.4 6.507 6.507 0 0 1 -3.018-5.49c0-3.584 2.916-6.5 6.5-6.5s6.5 2.916 6.5 6.5a6.513 6.513 0 0 1 -3.019 5.491 12.42 12.42 0 0 1 6.452 4.4c-2.328 2.925-5.912 4.809-9.933 4.809z"
-                      ></path>
-                    </svg>
-                  </div>
-                  <img class="profile-img" v-else :src="isLogedIn.imgUrl" />
                 </div>
-              </div>
-              <el-dropdown-menu class="hamburger-dropdown" slot="dropdown">
-                <el-dropdown-item
-                  v-if="!isLogedIn"
-                  icon="el-icon-circle-plus"
-                  command="loginModal"
-                  >Login</el-dropdown-item
-                >
-                <el-dropdown-item
-                  v-if="!isLogedIn"
-                  icon="el-icon-circle-plus"
-                  command="signUpModal"
-                  >Sign up</el-dropdown-item
-                >
-                <el-dropdown-item v-if="isLogedIn" icon="el-icon-circle-plus" command="toProfile"
-                  >My Profile</el-dropdown-item
-                >
-                <el-dropdown-item
-                  v-if="isLogedIn"
-                  icon="el-icon-circle-plus"
-                  command="logout"
-                  >Logout</el-dropdown-item
-                >
-              </el-dropdown-menu>
-            </el-dropdown>
-          </el-col>
-        </el-row>
+                <el-dropdown-menu class="hamburger-dropdown" slot="dropdown">
+                  <el-dropdown-item
+                    v-if="!isLogedIn"
+                    icon="el-icon-circle-plus"
+                    command="loginModal"
+                    >Login</el-dropdown-item
+                  >
+                  <el-dropdown-item
+                    v-if="!isLogedIn"
+                    icon="el-icon-circle-plus"
+                    command="signUpModal"
+                    >Sign up</el-dropdown-item
+                  >
+                  <el-dropdown-item v-if="isLogedIn" icon="el-icon-circle-plus" command="toProfile"
+                    >My Profile</el-dropdown-item
+                  >
+                  <el-dropdown-item
+                    v-if="isLogedIn"
+                    icon="el-icon-circle-plus"
+                    command="logout"
+                    >Logout</el-dropdown-item
+                  >
+                </el-dropdown-menu>
+              </el-dropdown>
+            </el-col>
+          </el-row>
+        </section>
       </div>
     </nav>
   </header>
@@ -110,6 +113,11 @@ import houseFilter from '@/cmps/houseFilter.cmp.vue'
 export default {
   components: {
     houseFilter,
+  },
+  data() {
+    return {
+      isDropdown: false
+    }
   },
   methods: {
     handleCommand(command) {
@@ -132,6 +140,10 @@ export default {
     toProfile() {
       this.$router.push('profile')
     },
+    onDropdown() {
+      this.isDropdown = !this.isDropdown;
+      console.log(this.isDropdown);
+    }
   },
   computed: {
     homeHeader() {
@@ -139,6 +151,9 @@ export default {
     },
     isLogedIn() {
       return this.$store.getters.loggedinUser
+    },
+    rotateBurger() {
+      return { 'hamburger-logo-trans': this.isDropdown }
     },
   }
 }
