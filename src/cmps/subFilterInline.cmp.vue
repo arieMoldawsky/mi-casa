407a026c917503314d0ef6b0c3bc1c3c676d4fbb
Tal Harouvi <halouvi@gmail.com>
2020-12-04 13:32:21 +0200

    filterfixes

---
+++ src/cmps/subFilterInline.cmp.vue
@@ -0,0 +1,34 @@
+<template>
+  <el-form
+    @submit.native.prevent="updateFilter"
+    class="sub-filter-inline f-as-jfs"
+    :model="filterBy"
+    size="medium"
+  >
+    <div class="item">Price</div>
+    <div class="item">Type</div>
+    <div class="item">Bedrooms</div>
+    <div class="item">Amenities</div>
+  </el-form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      filterBy: null,
+    }
+  },
+  methods: {
+    updateFilter() {},
+  },
+  computed: {
+    getFilterBy() {
+      return this.$store.getters.getFilterBy
+    },
+  },
+  created() {
+    this.filterBy = JSON.parse(JSON.stringify(this.getFilterBy))
+  },
+}
+</script>
